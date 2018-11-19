ccd57cd257c159cc20725f30152cbc4bab628d4e
Alex Itkes <38556752+alexitkes@users.noreply.github.com>
2018-11-19 09:58:33 +0300

    Add a TestCase class for select_features_ascending
    The tests are still to be written.

---
+++ tests/test_select_features.py
@@ -0,0 +1,12 @@
+"""
+Unit tests for `ai_kaggletools.select_features` module.
+"""
+
+from unittest import TestCase
+
+class TestAscending(TestCase):
+    """
+    Unit tests for `ai_kaggletools.select_features.select_features_ascending`
+    function.
+    """
+    pass
